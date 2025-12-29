301791b91491d018eaee6b27938429d0a6e434e0
OmSapkal9240 <omvilassapkal@gmail.com>
2025-12-29 19:29:35 +0530

    fix: vercel build, postcss cjs, tailwind cjs, clean setup

---
+++ src/context/LanguageContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext } from 'react';
+
+export const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('en');
+
+  const value = { language, setLanguage };
+
+  return (
+    <LanguageContext.Provider value={value}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (context === undefined) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from './App.jsx';
+import { LanguageProvider } from './context/LanguageContext.jsx';
 import "./index.css";
 import "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <LanguageProvider>
         <App />
+      </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
